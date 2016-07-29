b5d7ab535f1bd563d091959edaae180d6ad1c8bf
Flo <flooow18@gmail.com>
2016-07-29 13:27:31 +0200

    small improvements on index.cshtml

--- WeightControl/AngularControllers/IndexController.js
+++ WeightControl/AngularControllers/IndexController.js
@@ -16,16 +16,20 @@ function IndexController($scope, $http) {
     $scope.today();
 
 
-    //$scope.initDate = new Date('2016-15-20');
-    $scope.formats =['yyyy', 'dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.formats = ['yyyy', 'dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
     $scope.format = $scope.formats[0];
 
     $scope.datepickerOptions = {
-            datepickerMode: "'year'",
-            minMode: "'year'",
-            minDate: "minDate",
-            showWeeks: "false",
-        };
+        datepickerMode: "'year'",
+        minMode: "'year'",
+        minDate: "minDate",
+        showWeeks: "false",
+    };
+
+
+    $scope.monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
 
     init();
 
@@ -34,7 +38,7 @@ function IndexController($scope, $http) {
     }
 
     function getAllWeights() {
-        $http.get('/api/getAllWeights' + '/' + $scope.dt.getFullYear() + '/' + ($scope.dt.getMonth() +1).toString())
+        $http.get('/api/getAllWeights' + '/' + $scope.dt.getFullYear() + '/' + ($scope.dt.getMonth() + 1).toString())
             .then(function success(response) {
                 $scope.allWeights = response.data.Weights;
                 displayWeightTable(response.data.Weights);
@@ -59,28 +63,26 @@ function IndexController($scope, $http) {
         //    ]; //alert(tableData[0][1][0]); alerts '0.5'
 
         var options = {
-                //yaxis: { max: 10 },
-                    grid: {
-                    hoverable: true,
-                    clickable: true
-                },
-                    xaxis: {
-                    mode: "time",
-                    minTickSize: [1, "day"],
-                    min: (new Date(2016, 6, 30)).getTime(),
-                    max: (new Date(2016, 6, 1)).getTime(),
-                    timeformat: "%a"
-                }
-                }
+            //yaxis: { max: 10 },
+            grid: {
+                hoverable: true,
+                clickable: true
+            },
+            xaxis: {
+                tickSize: 1,
+                minTickSize: 1,
+                tickDecimals: 0
+            }
+        }
 
         var tableData = [];
 
-        Object.keys(weightData).forEach(function(element, index, array) {            // element is the date as key in the dictionairy from service
+        Object.keys(weightData).forEach(function (element, index, array) {            // element is the date as key in the dictionairy from service
             var dateString = element.toString().split('-');
             var year = dateString[0];
             var month = dateString[1];
             var day = dateString[2];
-            tableData.push([element, weightData[element]]); // replace index with element (contains the date yyyy-mm-dd)
+            tableData.push([day, weightData[element]]); 
         });
 
         $scope.allWeights = tableData;
@@ -89,28 +91,29 @@ function IndexController($scope, $http) {
         //tableData[0].push([0.5, 0.5]);
 
         $.plot($("#placeholder"), [{
-            data: tableData, label: "Gewicht" }], options);
+            data: tableData, label: "Gewicht"
+        }], options);
 
         $("<div id='tooltip'></div>").css({
-                position: "absolute",
-                display: "none",
-                border: "1px solid #fdd",
-                padding: "2px",
+            position: "absolute",
+            display: "none",
+            border: "1px solid #fdd",
+            padding: "2px",
             "background-color": "#fee",
-                opacity: 0.80
-            }).appendTo("body");
+            opacity: 0.80
+        }).appendTo("body");
 
         $("#placeholder").bind("plothover", function (event, pos, item) {
 
 
 
-            if(item) {
+            if (item) {
 
                 var date = item.datapoint[0];
                 var weightOnDate = item.datapoint[1];
 
-                $("#tooltip").html(item.series.label + ' am ' +date + ': ' + weightOnDate)
-                    .css({ top: item.pageY +5, left : item.pageX +5 })
+                $("#tooltip").html(item.series.label + ' am ' + date + ': ' + weightOnDate + 'kg')
+                    .css({ top: item.pageY + 5, left: item.pageX + 5 })
                     .fadeIn(200);
             } else {
                 $("#tooltip").hide();
